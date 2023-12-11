bbfdbf709b1e844f46462184a6a986532fcf3b67
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2023-12-11 16:39:06 -0300

    cors

--- API/src/app.js
+++ API/src/app.js
@@ -10,6 +10,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', ''); // REEMPLAZAR POR  http://localhost:5173 SI ESTAS DE FORMA LOCAL update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
