5239eca4036ba5881157b796f15b0f606e0f8ec2
Camilo Rodelo <camilo_jrl@hotmail.com>
2023-12-12 14:48:53 -0500

    comma-dangle change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import Navbar from './components/navbar/Navbar'
 import Card from './components/Card'
 import Login from './components/login/Login'
 
-import './App.css'
-
 function App() {
   return (
     <>
