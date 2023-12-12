fa7a6ad6c7ff7cbb3f76b5a278a6cc1d926b4de4
chrivel14 <chrivel1998@gmail.com>
2023-12-12 09:25:06 -0500

    cambio nullable el campo password
    Al manejar dos tipos de ingreso a la aplicación. Los registrados con Google no tendrán contraseña, por ello el campo podrá ser nulo

--- API/src/models/Users.js
+++ API/src/models/Users.js
@@ -21,7 +21,7 @@ module.exports = (sequelize) => {
     },
     password:{
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true
     },
     registration_type:{
         type: DataTypes.STRING,
