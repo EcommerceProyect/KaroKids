4b8edaf2fc08c8910c7ad8a45f1b2843f5a247fc
Camilo Rodelo <camilo_jrl@hotmail.com>
2023-12-11 22:18:36 -0500

    deleted login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import './App.css'
 
-import { Route, Routes } from 'react-router-dom'
-
 import Navbar from './components/navbar/Navbar'
-import Login from './views/login/Login'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
     </>
   )
 }
