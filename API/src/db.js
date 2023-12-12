bf2e737d045c6adf995d89c40a503a715ce639f6
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2023-12-11 22:42:29 -0300

    Implementacion de sequelize y coneccion con DB deployada en Render 1

--- API/src/db.js
+++ API/src/db.js
@@ -27,7 +27,7 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 Object.assign(sequelize.models, ...capsEntries);
 
-const { } = sequelize.models;
+// const { } = sequelize.models;
 
 module.exports = {
 
